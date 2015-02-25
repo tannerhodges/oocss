591cc859bb53cb2fb4ebe8cd6615386a94e6712a
Tanner Hodges <austintannerhodges@gmail.com>
2015-02-25 14:53:34 -0500

    Remove helper files and trim README

--- start/mac-start.command
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-directory="`dirname \"$0\"`"
-cd "$directory"
-cd ..
-vagrant up
-vagrant ssh
--- start/win-start.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-cd ..
-vagrant up
-
-@echo =========================================================
-@echo vagrant is stopped, you can now close the terminal window
-@echo and launch stop.bat for stopping the VM
